d38c7d1d7c81339098ef4be016e58e75c6fca852
venkatapavan22 <pavanvvp123@gmail.com>
2024-07-20 10:30:55 +0530

    Updated Community

--- backend/Routes/messageRoutes.js
+++ backend/Routes/messageRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 // import { verifyUser } from '../Middlewares/verifyToken.js';
-import { addConversation, deleteMessageById, getConversations } from '../Controllers/messageController.js';
+import { addConversation, deleteMessagesWhereUserIsNull, getConversations } from '../Controllers/messageController.js';
 
 
 const router = express.Router();
 
 router.post('/createMessage',addConversation);
 router.get('/getMessages',getConversations);
-router.delete('/deleteMessage/:id',deleteMessageById)
+router.delete('/deleteMessage/:id',deleteMessagesWhereUserIsNull)
 
 export default router;
